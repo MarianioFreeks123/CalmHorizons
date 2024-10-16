f53153f508564fd38d23b8229b35054e65129ced
MarianioFreeks123 <mariodecastroperdiguero@gmail.com>
2024-10-16 22:32:12 +0200

    AudioManager

--- Assets/Scripts/Main/Managers/AudioManager.cs
+++ Assets/Scripts/Main/Managers/AudioManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using UnityEngine.Audio;
 
 public enum SoundType
 {
@@ -21,42 +20,127 @@ public enum MusicType
 public class AudioManager : MonoBehaviour
 {
     [Header("SOUND LIST STRUCTURE")]
+    [Tooltip("Lista de sonidos organizados por tipo.")]
     [SerializeField] private SoundList[] soundList;
 
+    [Header("MUSIC LIST STRUCTURE")]
+    [Tooltip("Lista de pistas de música organizadas por tipo.")]
+    [SerializeField] private MusicList[] musicList;
+
     [Header("REFERENCES IN SCENE")]
-    [SerializeField] AudioSource soundAudioSource;
-    [SerializeField] AudioSource musicAudioSource;
+    [Tooltip("AudioSource para reproducir sonidos.")]
+    [SerializeField] private AudioSource soundAudioSource;
+    [Tooltip("AudioSource para reproducir música.")]
+    [SerializeField] private AudioSource musicAudioSource;
 
     public static AudioManager instance;
 
-    //Create the singleton
     private void Awake()
     {
         if (instance != null && instance != this)
         {
-            Destroy(this);
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject); // Mantener el AudioManager entre escenas
+        }
+    }
+
+    public static void PlaySound(SoundType sound, float volume = 1f)
+    {
+        SoundList selectedSound = Array.Find(instance.soundList, s => s.type == sound);
+
+        if (selectedSound.sounds.Length > 0)
+        {
+            AudioClip clip = selectedSound.sounds[UnityEngine.Random.Range(0, selectedSound.sounds.Length)];
+            instance.soundAudioSource.PlayOneShot(clip, selectedSound.volume * volume);
+        }
+        else
+        {
+            Debug.LogWarning("No sounds assigned to " + sound);
+        }
+    }
+
+    public static void PlayMusic(MusicType music, float volume = 1f, bool loop = true)
+    {
+        MusicList selectedMusic = Array.Find(instance.musicList, m => m.type == music);
+
+        if (selectedMusic.musicClip != null)
+        {
+            instance.musicAudioSource.clip = selectedMusic.musicClip;
+            instance.musicAudioSource.volume = selectedMusic.volume * volume;
+            instance.musicAudioSource.loop = loop;
+            instance.musicAudioSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("No music assigned to " + music);
+        }
+    }
+
+    public static IEnumerator FadeOutMusic(float fadeTime)
+    {
+        float startVolume = instance.musicAudioSource.volume;
+
+        while (instance.musicAudioSource.volume > 0)
+        {
+            instance.musicAudioSource.volume -= startVolume * Time.deltaTime / fadeTime;
+            yield return null;
         }
 
-        else instance = this;
+        instance.musicAudioSource.Stop();
+        instance.musicAudioSource.volume = startVolume;
     }
 
-    public static void PlaySound(SoundType sound, float volume = 1)
+    public static IEnumerator FadeInMusic(MusicType music, float fadeTime, float volume = 1f, bool loop = true)
     {
-        //AudioClip clips[] = instance.soundList[(int)sound.Sounds]
-        //instance.soundAudioSource.PlayOneShot(instance.soundList[(int)sound], volume);
-    }
+        PlayMusic(music, 0, loop); // Inicia la música con volumen cero
+        instance.musicAudioSource.volume = 0;
 
-    public static void PlayMusic(MusicType sound, float volume = 1)
-    {
-        //instance.musicAudioSource.volume = volume;
-        //instance.musicAudioSource.clip = instance.musicList[(int)sound];
+        while (instance.musicAudioSource.volume < volume)
+        {
+            instance.musicAudioSource.volume += Time.deltaTime / fadeTime;
+            yield return null;
+        }
     }
 }
 
 [Serializable]
 public struct SoundList
 {
-    public string name;
+    [Tooltip("Tipo de sonido (debe coincidir con el enum SoundType)")]
+    public SoundType type;
+
+    [Tooltip("Volumen general del sonido (entre 0 y 1)")]
     [Range(0, 1)] public float volume;
+
+    [Tooltip("Clips de audio asociados al tipo de sonido.")]
     public AudioClip[] sounds;
+
+    [Tooltip("Si se debe reproducir en bucle o no.")]
+    public bool loop;
+
+    [Tooltip("Si el sonido se reproduce inmediatamente al cargar la escena.")]
+    public bool playOnAwake;
+}
+
+[Serializable]
+public struct MusicList
+{
+    [Tooltip("Tipo de música (debe coincidir con el enum MusicType)")]
+    public MusicType type;
+
+    [Tooltip("Volumen general de la música (entre 0 y 1)")]
+    [Range(0, 1)] public float volume;
+
+    [Tooltip("Clip de audio de la música.")]
+    public AudioClip musicClip;
+
+    [Tooltip("Si la música se reproduce en bucle o no.")]
+    public bool loop;
+
+    [Tooltip("Si la música se reproduce inmediatamente al cargar la escena.")]
+    public bool playOnAwake;
 }
